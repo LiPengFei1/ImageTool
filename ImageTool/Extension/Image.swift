7648e3279c369b7146bb58f1754dbcb78cd21882
lipengfei <281005001@qq.com>
2016-11-07 16:22:42 +0800

    添加一个UIImage的扩展类

---
+++ ImageTool/Extension/Image.swift
@@ -0,0 +1,16 @@
+//
+//  Image.swift
+//  ImageTool
+//
+//  Created by 李鹏飞 on 16/11/7.
+//  Copyright © 2016年 com.lipengfei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIImage{
+
+    public func sizeToFit(){
+        print("这是一个测试方法")
+    }
+}
